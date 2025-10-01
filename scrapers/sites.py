db5be0970c361e489c7137db4fe36411ca2e9616
Ishat Noor Mahi <ishatnoormahi787@gmail.com>
2025-10-02 00:42:19 +0600

    site and region info

---
+++ scrapers/sites.py
@@ -0,0 +1,12 @@
+sites = {
+    'Bangladesh': {
+        'RoboShop': 'https://www.robo-shop.com/',
+        'StarTech': 'https://www.startech.com.bd/',
+        'TechLandBD': 'https://techlandbd.com/',
+    },
+    'USA': {
+        'Amazon': 'https://www.amazon.com/',
+        'Newegg': 'https://www.newegg.com/',
+        'BestBuy': 'https://www.bestbuy.com/',
+    }
+}
